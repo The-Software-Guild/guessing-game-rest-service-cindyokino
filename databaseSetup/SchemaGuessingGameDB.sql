ea44df152783e087d3564f08fc1ccb904632cf22
Cindy Okino <cindy.okino@gmail.com>
2020-12-18 12:38:35 -0500

    Create database schema: SchemaGuessingGameDB.sql

---
+++ databaseSetup/SchemaGuessingGameDB.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS guessingGameDB;
+
+CREATE DATABASE guessingGameDB;
+
+USE guessingGameDB;
+
+CREATE TABLE game (
+	gameId INT PRIMARY KEY AUTO_INCREMENT,
+    answer VARCHAR(4) NOT NULL,
+    `status` VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE round (
+	roundId INT PRIMARY KEY AUTO_INCREMENT,
+    guess VARCHAR(4) NOT NULL,
+    timeOfTheGuess TIME NOT NULL,
+    resultOfTheGuess VARCHAR(15),
+    gameId INT NOT NULL
+);
